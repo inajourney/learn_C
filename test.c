b708ccd48268cf41ed0574dcb19429df873c371b
mralpaca <77496184+inajourney@users.noreply.github.com>
2021-01-16 21:35:31 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -29,18 +29,33 @@
 //	return 0;//返回 0
 //}
 
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//
+//	//输入函数	
+//	scanf_s("%d%d",&num1, &num2);//&取地址符号
+//	//c语音语法规定，变量要定义在当前代码块的最前面
+//	sum = num1 + num2;
+//	printf("sum = %d\n", sum);
+//
+//
+//	return 0;
+//}
+
+int global = 2021;
+
+void test()
+{
+	printf("text()-%d\n", global);
+}
+
 int main()
 {
-	int num1 = 0;
-	int num2 = 0;
-	int sum = 0;
-
-	//输入函数	
-	scanf_s("%d%d",&num1, &num2);//&取地址符号
-	//c语音语法规定，变量要定义在当前代码块的最前面
-	sum = num1 + num2;
-	printf("sum = %d\n", sum);
-
+	test();
+	printf("%d\n", global);
 
 	return 0;
 }
